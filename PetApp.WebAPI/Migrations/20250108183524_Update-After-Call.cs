8167654dec347248d455da3851de6eaf44c7c2ec
Zhuk, Anton <Anton.Zhuk@thomsonreuters.com>
2025-01-08 22:44:51 +0400

    migrate schema to reflect requirement changes

---
+++ PetApp.WebAPI/Migrations/20250108183524_Update-After-Call.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PetApp.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAfterCall : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ads_Animals_AnimalId",
+                table: "Ads");
+
+            migrationBuilder.DropTable(
+                name: "AnimalSitters");
+
+            migrationBuilder.DropTable(
+                name: "UnavailableTimeframeSitters");
+
+            migrationBuilder.DropTable(
+                name: "Animals");
+
+            migrationBuilder.DropTable(
+                name: "UnavailableTimeframes");
+
+            migrationBuilder.DropColumn(
+                name: "AnimalAge",
+                table: "Ads");
+
+            migrationBuilder.DropColumn(
+                name: "AnimalBreed",
+                table: "Ads");
+
+            migrationBuilder.DropColumn(
+                name: "AnimalName",
+                table: "Ads");
+
+            migrationBuilder.DropColumn(
+                name: "AnimalPhoto",
+                table: "Ads");
+
+            migrationBuilder.DropColumn(
+                name: "VisitTime",
+                table: "Ads");
+
+            migrationBuilder.RenameColumn(
+                name: "AnimalId",
+                table: "Ads",
+                newName: "PetId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Ads_AnimalId",
+                table: "Ads",
+                newName: "IX_Ads_PetId");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "Sitters",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVisibleInSearch",
+                table: "Sitters",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Chats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SitterId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chats_Sitters_SitterId",
+                        column: x => x.SitterId,
+                        principalTable: "Sitters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Chats_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Breed = table.Column<string>(type: "text", nullable: false),
+                    Photo = table.Column<byte[]>(type: "bytea", nullable: false),
+                    Age = table.Column<int>(type: "integer", nullable: false),
+                    AdditionalInfo = table.Column<string>(type: "text", nullable: false),
+                    IsVisibleInSearch = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ChatId = table.Column<int>(type: "integer", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    DateTimeSent = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Chats_ChatId",
+                        column: x => x.ChatId,
+                        principalTable: "Chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PetSitters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SitterId = table.Column<int>(type: "integer", nullable: false),
+                    PetId = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetSitters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PetSitters_Pets_PetId",
+                        column: x => x.PetId,
+                        principalTable: "Pets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PetSitters_Sitters_SitterId",
+                        column: x => x.SitterId,
+                        principalTable: "Sitters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_SitterId",
+                table: "Chats",
+                column: "SitterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_UserId",
+                table: "Chats",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ChatId",
+                table: "Messages",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PetSitters_PetId",
+                table: "PetSitters",
+                column: "PetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PetSitters_SitterId",
+                table: "PetSitters",
+                column: "SitterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ads_Pets_PetId",
+                table: "Ads",
+                column: "PetId",
+                principalTable: "Pets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ads_Pets_PetId",
+                table: "Ads");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "PetSitters");
+
+            migrationBuilder.DropTable(
+                name: "Chats");
+
+            migrationBuilder.DropTable(
+                name: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "Sitters");
+
+            migrationBuilder.DropColumn(
+                name: "IsVisibleInSearch",
+                table: "Sitters");
+
+            migrationBuilder.RenameColumn(
+                name: "PetId",
+                table: "Ads",
+                newName: "AnimalId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Ads_PetId",
+                table: "Ads",
+                newName: "IX_Ads_AnimalId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AnimalAge",
+                table: "Ads",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AnimalBreed",
+                table: "Ads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AnimalName",
+                table: "Ads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "AnimalPhoto",
+                table: "Ads",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VisitTime",
+                table: "Ads",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UnavailableTimeframes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    End = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Start = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnavailableTimeframes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnimalSitters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AnimalId = table.Column<int>(type: "integer", nullable: false),
+                    SitterId = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnimalSitters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AnimalSitters_Animals_AnimalId",
+                        column: x => x.AnimalId,
+                        principalTable: "Animals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AnimalSitters_Sitters_SitterId",
+                        column: x => x.SitterId,
+                        principalTable: "Sitters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UnavailableTimeframeSitters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SitterId = table.Column<int>(type: "integer", nullable: false),
+                    UnavailableTimeframeId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnavailableTimeframeSitters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UnavailableTimeframeSitters_Sitters_SitterId",
+                        column: x => x.SitterId,
+                        principalTable: "Sitters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UnavailableTimeframeSitters_UnavailableTimeframes_Unavailab~",
+                        column: x => x.UnavailableTimeframeId,
+                        principalTable: "UnavailableTimeframes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnimalSitters_AnimalId",
+                table: "AnimalSitters",
+                column: "AnimalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnimalSitters_SitterId",
+                table: "AnimalSitters",
+                column: "SitterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UnavailableTimeframeSitters_SitterId",
+                table: "UnavailableTimeframeSitters",
+                column: "SitterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UnavailableTimeframeSitters_UnavailableTimeframeId",
+                table: "UnavailableTimeframeSitters",
+                column: "UnavailableTimeframeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ads_Animals_AnimalId",
+                table: "Ads",
+                column: "AnimalId",
+                principalTable: "Animals",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
